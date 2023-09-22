fa1e3e83f86fc02df161e0ff0a451a74b1e1f4cc
Desire Oluwarotimi <rotimidesire@gmail.com>
2023-09-22 03:03:01 +0100

    init

---
+++ main.js
@@ -0,0 +1,49 @@
+const KEY = "071270fb149a1d34a7f96bbe03e1b244";
+
+const form = document.querySelector("form");
+const city = document.getElementById("city");
+const weatherData = document.getElementById("data");
+
+async function getWeatherData(cityValue) {
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${KEY}&units=metric`
+    );
+
+    if (!res.ok) {
+      throw new Error("Invalid Network Response");
+    }
+
+    const data = await res.json();
+
+    const icon = data.weather[0].icon;
+    const temperature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}˚C`,
+      `Humidity: ${data.main.humidity}%`,
+      `Wind Speed: ${data.wind.speed}m/s`,
+    ];
+
+    weatherData.querySelector(
+      ".icon"
+    ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Icon" />`;
+
+    weatherData.querySelector(".temperature").textContent = `${temperature}˚C`;
+
+    weatherData.querySelector(".description").textContent = description;
+
+    weatherData.querySelector(".details").innerHTML = details.map(
+      (detail) => `<span>${detail}</span>`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const cityValue = city.value;
+  getWeatherData(cityValue);
+});
